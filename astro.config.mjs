a2fd19f8e70e07764209fbccfe3a2c089f075327
gangya0327 <king9999427@163.com>
2022-11-03 11:24:44 +0800

    my-website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import solidJs from "@astrojs/solid-js";
+import solidJs from '@astrojs/solid-js'
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  base: 'dist',
+  base: 'my-website',
   integrations: [solidJs(), tailwind()]
-});
+})
